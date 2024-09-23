2f53b80aaf849c1fd4f310e1f5239d9721e0b263
yamada <shinya.yamada@benext-corp.com>
2024-09-23 12:24:59 +0900

    ✨ Get mattar info from supabase

--- app/components/MatterCard.tsx
+++ app/components/MatterCard.tsx
@@ -1,55 +1,53 @@
-"use client";
+import {
+  Card,
+  Text,
+  Badge,
+  Button,
+  Group,
+  Container,
+  SimpleGrid,
+} from "@mantine/core";
+import { Database } from "../lib/database.types";
 
-import { Card, Text, Badge, Button, Group } from "@mantine/core";
+type MattersTable = Database["public"]["Tables"]["matters"];
+type MattersRow = MattersTable["Row"];
 
-export function MatterCardsGrid() {
+export function MatterCardsGrid({
+  matterList,
+}: {
+  matterList: MattersRow[] | null;
+}) {
+  const cards = matterList?.map((matter) => (
+    <Card
+      key={matter.name}
+      p="md"
+      radius="md"
+      component="a"
+      className="hover:bg-transparent hover:cursor-pointer hover:bg-gray-100 border transition"
+      shadow="sm"
+      //   onClick={() => openCard(matter)}
+    >
+      <Group justify="space-between" align="flex-start">
+        <Text fw={700} size="lg" mt={5}>
+          {matter.name}
+        </Text>
+        {matter.is_fixed ? (
+          <Badge color="blue">確定</Badge>
+        ) : (
+          <Badge color="red">未確定</Badge>
+        )}
+      </Group>
+      <Text className="">分類：{matter.category}</Text>
+      <Text className="">金額：{matter.amount}円</Text>
+      <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
+        {matter.inserted_at}
+      </Text>
+    </Card>
+  ));
+
   return (
-    <div className="flex justify-between mx-4 gap-6">
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink">On Sale</Badge>
-        </Group>
-
-        <Text size="sm" c="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes
-          with tours and activities on and around the fjords of Norway
-        </Text>
-
-        <Button color="blue" fullWidth mt="md" radius="md">
-          Book classic tour now
-        </Button>
-      </Card>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink">On Sale</Badge>
-        </Group>
-
-        <Text size="sm" c="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes
-          with tours and activities on and around the fjords of Norway
-        </Text>
-
-        <Button color="blue" fullWidth mt="md" radius="md">
-          Book classic tour now
-        </Button>
-      </Card>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink">On Sale</Badge>
-        </Group>
-
-        <Text size="sm" c="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes
-          with tours and activities on and around the fjords of Norway
-        </Text>
-
-        <Button color="blue" fullWidth mt="md" radius="md">
-          Book classic tour now
-        </Button>
-      </Card>
-    </div>
+    <Container py="xl">
+      <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
+    </Container>
   );
 }
