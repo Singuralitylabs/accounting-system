e4380a9da2a7d0c2ff3d269937cdae521dc60f10
山田慎也 <s-yamada@future-tech-association.org>
2024-09-23 03:53:07 +0000

    Add force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import PageTitle from "./components/PageTitle";
 import { createServerComponentClient } from "@/node_modules/@supabase/auth-helpers-nextjs/dist/index";
 import { cookies } from "@/node_modules/next/headers";
 
+// 明示的に動的なページに設定する
+export const dynamic = "force-dynamic";
+
 const UsersMatterPage = async () => {
   const supabase = createServerComponentClient({ cookies });
   const { data: matterList } = await supabase.from("matters").select("*");
 
   return (
-    <main className="">
+    <main>
       <PageTitle title="案件一覧" />
       <MatterCardsGrid matterList={matterList} />
     </main>
