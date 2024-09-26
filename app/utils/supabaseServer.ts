7f78653e929c04846aa93facd27fa7de1e89c052
山田慎也 <s-yamada@future-tech-association.org>
2024-09-26 23:26:37 +0000

    Add update & delete matterInfo function

--- app/utils/supabaseServer.ts
+++ app/utils/supabaseServer.ts
@@ -1,6 +1,9 @@
+"use server";
+
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "../lib/database.types";
+import { MatterType } from "../types/types";
 
 export const supabaseServer = () => {
   cookies().getAll();
@@ -12,4 +15,24 @@ export const getUserMatterInfoList = async () => {
   const { data: matterList } = await supabase.from("matters").select("*");
 
   return matterList;
+};
+
+export const updateMatterInfoInSupabase = async (matterInfo: MatterType) => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data: status, error } = await supabase
+    .from("matters")
+    .update(matterInfo)
+    .eq("id", matterInfo.id);
+
+  return { status, error };
+};
+
+export const deleteMatterInfoInSupabase = async (id: number) => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data: status, error } = await supabase
+    .from("matters")
+    .delete()
+    .eq("id", id);
+
+  return { status, error };
 };
