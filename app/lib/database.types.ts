b08793cba50c3ffc8075098a20bb07259e56bfdc
yamada <shinya.yamada@benext-corp.com>
2024-09-27 09:56:58 +0900

    🏷️ Add cost type

--- app/lib/database.types.ts
+++ app/lib/database.types.ts
@@ -4,108 +4,161 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
-      matters: {
+      costs: {
         Row: {
-          amount: number | null
-          billing_address: string | null
-          category: string
-          id: number
-          inserted_at: string
-          invoice_date: string | null
-          is_completed: boolean | null
-          is_fixed: boolean | null
-          name: string
-          period_date: string | null
-          start_date: string | null
-          team: string
-          updated_at: string
-          user_id: number
-        }
+          amount: number;
+          certificate: string;
+          comment: string | null;
+          id: number;
+          inserted_at: string;
+          item: string;
+          matter_id: number;
+          name: string;
+          payment_target: string;
+          period: string | null;
+          updated_at: string;
+          withholding: boolean;
+        };
         Insert: {
-          amount?: number | null
-          billing_address?: string | null
-          category: string
-          id?: number
-          inserted_at?: string
-          invoice_date?: string | null
-          is_completed?: boolean | null
-          is_fixed?: boolean | null
-          name: string
-          period_date?: string | null
-          start_date?: string | null
-          team: string
-          updated_at?: string
-          user_id: number
-        }
+          amount: number;
+          certificate: string;
+          comment?: string | null;
+          id?: number;
+          inserted_at?: string;
+          item: string;
+          matter_id: number;
+          name: string;
+          payment_target: string;
+          period?: string | null;
+          updated_at?: string;
+          withholding: boolean;
+        };
         Update: {
-          amount?: number | null
-          billing_address?: string | null
-          category?: string
-          id?: number
-          inserted_at?: string
-          invoice_date?: string | null
-          is_completed?: boolean | null
-          is_fixed?: boolean | null
-          name?: string
-          period_date?: string | null
-          start_date?: string | null
-          team?: string
-          updated_at?: string
-          user_id?: number
-        }
+          amount?: number;
+          certificate?: string;
+          comment?: string | null;
+          id?: number;
+          inserted_at?: string;
+          item?: string;
+          matter_id?: number;
+          name?: string;
+          payment_target?: string;
+          period?: string | null;
+          updated_at?: string;
+          withholding?: boolean;
+        };
         Relationships: [
           {
-            foreignKeyName: "matter_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: "costs_matter_id_fkey";
+            columns: ["matter_id"];
+            isOneToOne: false;
+            referencedRelation: "matters";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      matters: {
+        Row: {
+          amount: number | null;
+          billing_address: string | null;
+          category: string;
+          id: number;
+          inserted_at: string;
+          invoice_date: string | null;
+          is_completed: boolean | null;
+          is_fixed: boolean | null;
+          name: string;
+          period_date: string | null;
+          start_date: string | null;
+          team: string;
+          updated_at: string;
+          user_id: number;
+        };
+        Insert: {
+          amount?: number | null;
+          billing_address?: string | null;
+          category: string;
+          id?: number;
+          inserted_at?: string;
+          invoice_date?: string | null;
+          is_completed?: boolean | null;
+          is_fixed?: boolean | null;
+          name: string;
+          period_date?: string | null;
+          start_date?: string | null;
+          team: string;
+          updated_at?: string;
+          user_id: number;
+        };
+        Update: {
+          amount?: number | null;
+          billing_address?: string | null;
+          category?: string;
+          id?: number;
+          inserted_at?: string;
+          invoice_date?: string | null;
+          is_completed?: boolean | null;
+          is_fixed?: boolean | null;
+          name?: string;
+          period_date?: string | null;
+          start_date?: string | null;
+          team?: string;
+          updated_at?: string;
+          user_id?: number;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "matter_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
       profiles: {
         Row: {
-          class: string | null
-          email: string
-          id: number
-          name: string
-        }
+          class: string | null;
+          email: string;
+          id: number;
+          name: string;
+        };
         Insert: {
-          class?: string | null
-          email: string
-          id?: never
-          name: string
-        }
+          class?: string | null;
+          email: string;
+          id?: never;
+          name: string;
+        };
         Update: {
-          class?: string | null
-          email?: string
-          id?: never
-          name?: string
-        }
-        Relationships: []
-      }
-    }
+          class?: string | null;
+          email?: string;
+          id?: never;
+          name?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -114,23 +167,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+      PublicSchema["Views"])
+  ? (PublicSchema["Tables"] &
+      PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -138,20 +191,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -159,20 +212,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -180,9 +233,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+  : never;
