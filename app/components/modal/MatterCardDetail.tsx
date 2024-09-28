274ffd68e166c5b21d76c4ecd46bd8c0e73596c7
yamada <shinya.yamada@benext-corp.com>
2024-09-28 10:08:03 +0900

    ✨ Add costInfo

--- app/components/modal/MatterCardDetail.tsx
+++ app/components/modal/MatterCardDetail.tsx
@@ -1,7 +1,8 @@
-import { categoryList, teamList } from "@/app/types/params";
-import { MatterType } from "@/app/types/types";
+import { categoryList, itemList, teamList } from "@/app/types/params";
+import { CostType, MatterType } from "@/app/types/types";
 import {
   deleteMatterInfoInSupabase,
+  getUserCostInfoList,
   updateMatterInfoInSupabase,
 } from "@/app/utils/supabaseServer";
 import {
@@ -14,6 +15,7 @@ import {
   Group,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useEffect, useState } from "react";
 
 type Props = {
   matterInfo: MatterType;
@@ -35,6 +37,25 @@ export const MatterCardDetailModal = ({
   opened,
   setOpened,
 }: Props) => {
+  const [costInfoList, setCostInfoList] = useState<CostType[]>([]);
+  useEffect(() => {
+    if (opened) {
+      const getCostInfo = async () => {
+        const { costInfoList, error } = await getUserCostInfoList(
+          matterInfo.id
+        );
+
+        if (error) {
+          console.error("Error fetching costInfoList:", error);
+          return <div>コスト情報の取得に失敗しました。</div>;
+        } else if (costInfoList) {
+          setCostInfoList(costInfoList);
+        }
+      };
+      getCostInfo();
+    }
+  }, [opened]);
+
   const form = useForm({
     initialValues: {
       id: matterInfo.id,
@@ -43,6 +64,7 @@ export const MatterCardDetailModal = ({
       category: matterInfo.category,
       amount: matterInfo.amount,
       is_fixed: matterInfo.is_fixed,
+      costInfoList: costInfoList as CostType[],
     },
   });
 
@@ -111,6 +133,61 @@ export const MatterCardDetailModal = ({
           label="確定"
           {...form.getInputProps("isFixed", { type: "checkbox" })}
         />
+        {costInfoList.length > 0 ? <h2 className="my-4">コスト情報</h2> : ""}
+        {costInfoList?.map((cost) => (
+          <div className="flex items-center pb-2" key={cost.id}>
+            <Group gap="sm" className="flex-grow" grow>
+              <TextInput
+                placeholder="品名をご記入ください。"
+                className="flex-grow"
+                value={cost.name as string}
+                onChange={(event) =>
+                  setCostInfoList(
+                    costInfoList.map((costInfo) =>
+                      costInfo.id === cost.id
+                        ? { ...costInfo, name: event.target.value }
+                        : costInfo
+                    )
+                  )
+                }
+              />
+              <Select
+                className="flex-grow"
+                placeholder="品目を選択ください。"
+                data={itemList}
+                required
+                value={cost.item}
+                onChange={(value) =>
+                  setCostInfoList(
+                    costInfoList.map((costInfo) =>
+                      costInfo.id === cost.id
+                        ? { ...costInfo, item: value || "" }
+                        : costInfo
+                    )
+                  )
+                }
+              />
+              <NumberInput
+                placeholder="¥0"
+                className="flex-grow"
+                value={cost.amount as number}
+                prefix="¥"
+                allowNegative={false}
+                allowDecimal={false}
+                thousandSeparator=","
+                onChange={(value) =>
+                  setCostInfoList(
+                    costInfoList.map((costInfo) =>
+                      costInfo.id === cost.id
+                        ? { ...costInfo, price: Number(value) }
+                        : costInfo
+                    )
+                  )
+                }
+              />
+            </Group>
+          </div>
+        ))}
         <div className="border-spacing-3 h-6"></div>
         <div className="flex justify-between">
           <Group justify="flex-end" mt="md">
