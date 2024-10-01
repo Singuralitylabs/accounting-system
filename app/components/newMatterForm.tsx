c19c8109c2faa5d9e1a19a26d7810f128ccceeb3
yamada <shinya.yamada@benext-corp.com>
2024-10-01 13:51:55 +0900

    ✨ Add handleAddMatterInfo function

--- app/components/newMatterForm.tsx
+++ app/components/newMatterForm.tsx
@@ -26,42 +26,29 @@ import {
   insertMatterInfoInSupabase,
 } from "../utils/supabaseServer";
 
-type InputCostType = {
-  id: number;
-  name: string;
-  item: string;
-  price: number;
-};
-
 const NewMatterForm = () => {
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
+      id: 0,
       title: "",
-      classification: "",
-      billing_amount: 0,
-      isFixed: false,
+      category: "",
+      amount: 0,
+      team: "",
+      billing_address: "",
+      start_date: "",
+      invoice_date: "",
+      period_date: "",
+      description: "",
+      is_fixed: false,
+      is_completed: false,
+      user_id: 0,
+      inserted_at: "",
+      updated_at: "",
       costList: [],
     },
   });
 
-  const [matterInfo, setMatterInfo] = useState<MatterType>({
-    id: 0,
-    title: "",
-    category: "",
-    team: "",
-    amount: 0,
-    is_fixed: false,
-    is_completed: false,
-    inserted_at: "",
-    user_id: 0,
-    billing_address: "",
-    invoice_date: "",
-    period_date: "",
-    start_date: "",
-    updated_at: "",
-    description: "",
-  });
   const [costList, setCostList] = useState<CostType[]>([]);
   const [costIndex, setCostIndex] = useState<number>(1);
 
@@ -90,35 +77,51 @@ const NewMatterForm = () => {
     setCostList(costList.filter((cost) => cost.id !== id));
   };
 
-  const handleAddMatterInfo = async () => {
-    // try {
-    //   const { newId, error: matterError } = await insertMatterInfoInSupabase(matterInfo);
-    //   if (matterError) throw new Error(matterError.message);
-    //   for (const cost of costList) {
-    //     const costInfo: CostType = {
-    //       id: 0,
-    //       name: cost.name,
-    //       price: cost.price,
-    //       item: cost.item,
-    //       matter_id: newId,
-    //       inserted_at: "",
-    //     };
-    //     const { error: costError } = await insertCostInfoInSupabase(costInfo);
-    //     if (costError) throw new Error(costError.message);
-    //   }
-    //   alert(`${matterInfo.title}の新規登録を完了しました。`);
-    //   form.reset();
-    //   setCostList([{ id: 0, name: "", item: "", price: 0 }]);
-    // } catch (error) {
-    //   alert(`${matterInfo.title}の新規登録に失敗しました。`);
-    //   console.error(error);
-    // }
+  const handleAddMatterInfo = async (matterInfo: MatterType) => {
+    try {
+      console.log(JSON.stringify(matterInfo));
+      const { newId, error: matterError } = await insertMatterInfoInSupabase(
+        matterInfo.title,
+        matterInfo.category,
+        matterInfo.team,
+        matterInfo.amount,
+        matterInfo.billing_address!,
+        matterInfo.start_date!,
+        matterInfo.invoice_date,
+        matterInfo.period_date,
+        matterInfo.is_fixed!,
+        matterInfo.description
+      );
+      console.log(newId);
+      if (matterError) throw new Error(matterError.message);
+      if (!newId) throw new Error("案件IDの取得に失敗しました。");
+      for (const cost of costList) {
+        const { error: costError } = await insertCostInfoInSupabase(
+          cost.name,
+          cost.item,
+          cost.payment_target,
+          cost.price,
+          cost.period!,
+          cost.certificate,
+          cost.withholding,
+          newId,
+          cost.comment!
+        );
+        if (costError) throw new Error(costError.message);
+      }
+      alert(`${matterInfo.title}の新規登録を完了しました。`);
+      form.reset();
+      setCostList([]);
+    } catch (error) {
+      alert(`${matterInfo.title}の新規登録に失敗しました。`);
+      console.error(error);
+    }
   };
 
   return (
     <form
       className="p-4 lg:p-16 w-auto"
-      onSubmit={form.onSubmit((values) => console.log(values))}
+      onSubmit={form.onSubmit((values) => handleAddMatterInfo(values))}
     >
       <div className="sm:flex gap-4 w-full">
         <TextInput
@@ -129,9 +132,6 @@ const NewMatterForm = () => {
           label="案件名"
           key={form.key("title")}
           {...form.getInputProps("title")}
-          onChange={(event) =>
-            setMatterInfo({ ...matterInfo, title: event.currentTarget.value })
-          }
         />
         <TextInput
           className="w-full"
@@ -139,12 +139,6 @@ const NewMatterForm = () => {
           label="請求先"
           key={form.key("billing_address")}
           {...form.getInputProps("billing_address")}
-          onChange={(event) =>
-            setMatterInfo({
-              ...matterInfo,
-              billing_address: event.currentTarget.value,
-            })
-          }
         />
       </div>
       <div className="sm:flex gap-4 w-full">
@@ -155,9 +149,8 @@ const NewMatterForm = () => {
           label="分類"
           data={categoryList}
           clearable
-          onChange={(event) =>
-            event ? setMatterInfo({ ...matterInfo, category: event }) : null
-          }
+          key={form.key("category")}
+          {...form.getInputProps("category")}
         />
         <Select
           withAsterisk
@@ -166,9 +159,8 @@ const NewMatterForm = () => {
           label="チーム"
           data={teamList}
           clearable
-          onChange={(event) =>
-            event ? setMatterInfo({ ...matterInfo, team: event }) : null
-          }
+          key={form.key("team")}
+          {...form.getInputProps("team")}
         />
         <NumberInput
           withAsterisk
@@ -181,9 +173,6 @@ const NewMatterForm = () => {
           thousandSeparator=","
           key={form.key("amount")}
           {...form.getInputProps("amount")}
-          onChange={(event) =>
-            setMatterInfo({ ...matterInfo, amount: Number(event) })
-          }
         />
       </div>
 
@@ -194,30 +183,24 @@ const NewMatterForm = () => {
           label="案件開始日"
           placeholder="案件を開始した日付をご入力ください。"
           valueFormat="YYYY/MM/DD"
-          onChange={(event) => {
-            const dateString = event ? event.toISOString().split("T")[0] : null;
-            setMatterInfo({ ...matterInfo, start_date: dateString });
-          }}
+          key={form.key("start_date")}
+          {...form.getInputProps("start_date")}
         />
         <DateInput
           className="pt-4 w-full"
           label="請求日"
           placeholder="案件の報酬を請求する日付をご入力ください。"
           valueFormat="YYYY/MM/DD"
-          onChange={(event) => {
-            const dateString = event ? event.toISOString().split("T")[0] : null;
-            setMatterInfo({ ...matterInfo, start_date: dateString });
-          }}
+          key={form.key("invoice_date")}
+          {...form.getInputProps("invoice_date")}
         />
         <DateInput
           className="pt-4 w-full"
           label="振込期限"
           placeholder="案件の報酬の振込期限をご入力ください。"
           valueFormat="YYYY/MM/DD"
-          onChange={(event) => {
-            const dateString = event ? event.toISOString().split("T")[0] : null;
-            setMatterInfo({ ...matterInfo, start_date: dateString });
-          }}
+          key={form.key("period_date")}
+          {...form.getInputProps("period_date")}
         />
       </div>
 
@@ -227,12 +210,6 @@ const NewMatterForm = () => {
         label="説明"
         key={form.key("description")}
         {...form.getInputProps("description")}
-        onChange={(event) =>
-          setMatterInfo({
-            ...matterInfo,
-            description: event.currentTarget.value,
-          })
-        }
       />
 
       <div className="pt-4">
@@ -410,9 +387,7 @@ const NewMatterForm = () => {
       </div>
 
       <Group className="pt-8" justify="flex-end" mt="md">
-        <Button type="submit" onClick={handleAddMatterInfo}>
-          作成
-        </Button>
+        <Button type="submit">作成</Button>
       </Group>
     </form>
   );
