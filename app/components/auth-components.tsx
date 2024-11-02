443b40b06371fdb45c85d010264abeee077c2ffe
yamada <shinya.yamada@benext-corp.com>
2024-11-02 18:17:08 +0900

    ⚡️ Add routing to top when signin

--- app/components/auth-components.tsx
+++ app/components/auth-components.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export const SignIn = () => {
   const supabase = createClientComponentClient();
+  const router = useRouter();
 
   const handleSignIn = async () => {
     try {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (session) {
+        router.push("/");
+        return;
+      }
+
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
@@ -20,6 +31,7 @@ export const SignIn = () => {
 
       if (error) {
         console.error("認証エラー:", error.message);
+        throw error;
       }
     } catch (error) {
       console.error("ログイン処理でエラーが発生しました:", error);
