244eccad788c617617d0b73f83fa632af205d573
yamada <shinya.yamada@benext-corp.com>
2025-01-27 08:47:46 +0900

    🐛 Correct unchecked_cost_count in insertMatter

--- app/utils/supabase/supabaseServer.ts
+++ app/utils/supabase/supabaseServer.ts
@@ -256,7 +256,7 @@ export const insertMatterInfo = async (
         business_count: business_count,
         total_cost: total_cost,
         cost_count: cost_count,
-        unchecked_cost_count: 0,
+        unchecked_cost_count: cost_count,
         is_fixed: is_fixed,
         is_completed: false,
         user_id: profileInfo.id,
